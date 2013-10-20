92b0cf7ce3091ff49fc684054d4b1785a386333b
Zack Piispanen <olink@shankshock.com>
2013-10-20 06:49:17 -0400

    Initial Commit

---
+++ MultiServerChat/Config.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MultiServerChat
+{
+	class ConfigFile
+	{
+		public string RestURL = "http://127.0.0.1:7878/msc";
+		public string Token = "abcdef";
+		public string ChatFormat = "[{0}]{2}{3}{4}: {5}";
+        /// <summary>
+        /// Reads a configuration file from a given path
+        /// </summary>
+        /// <param name="path">string path</param>
+        /// <returns>ConfigFile object</returns>
+        public static ConfigFile Read(string path)
+		{
+			if (!File.Exists(path))
+				return new ConfigFile();
+			using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+			{
+				return Read(fs);
+			}
+		}
+
+        /// <summary>
+        /// Reads the configuration file from a stream
+        /// </summary>
+        /// <param name="stream">stream</param>
+        /// <returns>ConfigFile object</returns>
+		public static ConfigFile Read(Stream stream)
+		{
+			using (var sr = new StreamReader(stream))
+			{
+				var cf = JsonConvert.DeserializeObject<ConfigFile>(sr.ReadToEnd());
+				if (ConfigRead != null)
+					ConfigRead(cf);
+				return cf;
+			}
+		}
+
+        /// <summary>
+        /// Writes the configuration to a given path
+        /// </summary>
+        /// <param name="path">string path - Location to put the config file</param>
+		public void Write(string path)
+		{
+			using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+			{
+				Write(fs);
+			}
+		}
+
+        /// <summary>
+        /// Writes the configuration to a stream
+        /// </summary>
+        /// <param name="stream">stream</param>
+		public void Write(Stream stream)
+		{
+			var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+			using (var sw = new StreamWriter(stream))
+			{
+				sw.Write(str);
+			}
+		}
+
+        /// <summary>
+        /// On config read hook
+        /// </summary>
+		public static Action<ConfigFile> ConfigRead;
+	}
+}
---
+++ MultiServerChat/MultiServerChat.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using HttpServer;
+using Newtonsoft.Json;
+using Rests;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace MultiServerChat
+{
+	[ApiVersion(1,14)]
+	public class MultiServerChat : TerrariaPlugin
+	{
+		ConfigFile Config = new ConfigFile();
+		private string savePath = "";
+		public MultiServerChat(Main game) : base(game)
+		{
+			savePath = Path.Combine(TShock.SavePath, "multiserverchat.json");
+			Config = ConfigFile.Read(savePath);
+			Config.Write(savePath);
+		}
+
+		public override void Initialize()
+		{
+			ServerApi.Hooks.ServerChat.Register(this, OnChat, 10);
+			TShock.RestApi.Register(new SecureRestCommand("/msc", RestChat, "msc.canchat"));
+		}
+
+		private object RestChat(RestVerbs verbs, IParameterCollection parameters, SecureRest.TokenData tokenData)
+		{
+			if (!string.IsNullOrWhiteSpace(parameters["message"]))
+			{
+				var bytes = Convert.FromBase64String(parameters["message"]);
+				var str = Encoding.UTF8.GetString(bytes);
+				var message = Message.FromJson(str);
+				TShock.Utils.Broadcast(message.Text, message.Red, message.Green, message.Blue);
+			}
+
+			return new RestObject();
+		}
+
+		private void OnChat(ServerChatEventArgs args)
+		{
+			var tsplr = TShock.Players[args.Who];
+			if (tsplr == null)
+			{
+				return;
+			}
+
+			if (args.Text.StartsWith("/") && args.Text.Length > 1)
+			{
+			}
+			else
+			{
+				if (!tsplr.Group.HasPermission(Permissions.canchat))
+				{
+					return;
+				}
+
+				if (tsplr.mute)
+				{
+					return;
+				}
+
+				var message = new Message()
+				{
+					Text =
+						String.Format(Config.ChatFormat, TShock.Config.ServerName, tsplr.Group.Name, tsplr.Group.Prefix, tsplr.Name, tsplr.Group.Suffix,
+							args.Text),
+					Red = tsplr.Group.R,
+					Green = tsplr.Group.G,
+					Blue = tsplr.Group.B
+				};
+
+				var bytes = Encoding.UTF8.GetBytes(message.ToString());
+				var base64 = Convert.ToBase64String(bytes);
+
+				var uri = String.Format("{0}?message={1}&token={2}", Config.RestURL, base64, Config.Token);
+
+				var request = (HttpWebRequest)WebRequest.Create(uri);
+				request.GetResponse();
+			}
+		}
+	}
+
+	public class Message
+	{
+		public string Text { get; set; }
+		public byte Red { get; set; }
+		public byte Green { get; set; }
+		public byte Blue { get; set; }
+
+		public override string ToString()
+		{
+			return JsonConvert.SerializeObject(this, Formatting.Indented);
+		}
+
+		public static Message FromJson(string js)
+		{
+			return JsonConvert.DeserializeObject<Message>(js);
+		}
+	}
+}
